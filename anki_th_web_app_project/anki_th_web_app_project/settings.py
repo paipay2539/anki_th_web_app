e0fd62140fc0998aa9a8bebbe1cc91d5ad2f2782
paidev2539 <paidev2539@gmail.com>
2021-10-17 18:29:37 +0700

    add static

--- anki_th_web_app_project/anki_th_web_app_project/settings.py
+++ anki_th_web_app_project/anki_th_web_app_project/settings.py
@@ -126,3 +126,15 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+# for static file
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "card_generator//static"),
+]
