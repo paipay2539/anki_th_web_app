683e37635cdcba3435ff3adba9a26028af3593a2
paidev2539 <paidev2539@gmail.com>
2021-10-21 20:20:44 +0700

    commit

--- anki_th_web_app_project/card_generator/views.py
+++ anki_th_web_app_project/card_generator/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render
 
 #from .models import Post
 
+
+import sys 
+sys.path.append('../reference')
+import ankiTH
+
+from django.shortcuts import redirect
+
 from django.http import HttpResponse
 
 def home_page(request):
@@ -16,8 +23,25 @@ def guide_page(request):
 
 def try_it_page(request):
     # return HttpResponse("w")
+
+    
+    for key, value in request.POST.items():
+        print('Key: %s' % (key) ) 
+        # print(f'Key: {key}') in Python >= 3.7
+        print('Value %s' % (value) )
+        # print(f'Value: {value}') in Python >= 3.7
+    if request.method == 'GET':
+        print("welcome to try_it_page")
+    elif request.method == 'POST':
+        print("post")
+    
     return render(request, "card_generator/try_it.html")
 
 def shared_deck_page(request):
     # return HttpResponse("w")
     return render(request, "card_generator/shared_deck.html")
+
+def generate_output(request):
+    print("pressed")
+    return HttpResponse("w")
+    return render(request, "card_generator/try_it.html", {"output_text":"hello"})
--- env_setup_server_start_script.bat
+++ env_setup_server_start_script.bat
@@ -1,2 +1,2 @@
 cd %CD%\anki_th_web_app_env
-cmd /k "Scripts\activate && cd .. && cd /d anki_th_web_app_project & python manage.py runserver 0.0.0.0:8000"
+cmd /k "Scripts\activate && cd .. && cd /d anki_th_web_app_project & python manage.py runserver 0.0.0.0:8000 && cd .."
